64121016be75ef1c05df7afeb09468fc91f85b6d
Basil <mbonubasil@gmail.com>
2018-10-22 09:20:28 +0100

    Add db setup

---
+++ server/config/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const db = mongoose.connection;
+
+
+mongoose.connect(process.env.DB || 'mongodb://localhost/dev-contact');
+db.on('connected', () => console.log('db connected successfully'));
+db.on('error', () => console.log('Ooops! something went wrong with db connection'));
+
+
+module.exports = db;
