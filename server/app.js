5c9ab38ad7a796efdf7bd2edfc19d5248fa2db07
Basil <mbonubasil@gmail.com>
2018-10-22 01:06:17 +0100

    Remove all routes from app.js

--- server/app.js
+++ server/app.js
@@ -4,10 +4,10 @@ const router = require('./routes');
 const app = express();
 
 
-app.get('/', (req, res) => {
-    res.send('Hello dev api');
-});
 
 app.use('/api/v1/', router);
 
+
+
 const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`app started on port ${port} ...`));
--- server/routes.js
+++ server/routes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 
+router.get('/', (req, res) => {
+    res.send('Hello dev api');
+});
+
 router.get('/developers', (req, res) => {
     res.send('hi developers');
 });
